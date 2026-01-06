3c0047b55539cf737c23da2d4bc358720717eafc
Subaika Velsamy <subaika15@gmail.com>
2026-01-06 12:00:01 +0530

    Updated 6Jan2026

--- ecommerce_backends/settings.py
+++ ecommerce_backends/settings.py
@@ -155,8 +155,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = 'subaika15@gmail.com'
-EMAIL_HOST_PASSWORD = 'uyjw mgzn rlxz rfsx'
+EMAIL_HOST_USER = 'your mailid'
+EMAIL_HOST_PASSWORD = 'your app id'
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
